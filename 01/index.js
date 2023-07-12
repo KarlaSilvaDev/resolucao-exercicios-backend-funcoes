40687e61eb553874f92bb1beebd249ccb8e9e80a
Karlasilvaeng <karlasilvaeng@gmail.com>
2023-07-12 17:32:33 -0300

    Ex01 - Sistema de Correção de Provas

--- 01/index.js
+++ 01/index.js
@@ -26,3 +26,17 @@ const prova = {
     ]
 };
 
+function corrigirProva(prova){
+    const {aluno, materia, valor, questoes} = prova; //desestruturação
+    let acertos = 0;
+    for (let questao of questoes){             
+        if (questao.resposta === questao.correta){ 
+            acertos++;
+        }
+    }
+    let nota = (valor/questoes.length)*acertos; //calcula o valor de cada questão e multiplica pela quantidade de acertos.
+    console.log(`O aluno(a) ${aluno} acertou ${acertos} ${acertos > 1 ? "questões" : "questão"} e obteve nota ${nota} na prova de ${materia}.` );
+}
+
+corrigirProva(prova);
+
