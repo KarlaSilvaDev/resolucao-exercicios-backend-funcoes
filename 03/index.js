07ddfa094b57d11167ce5b8afc953c7ddf3bbd6b
Karlasilvaeng <karlasilvaeng@gmail.com>
2023-07-12 18:42:46 -0300

    Implementação da função addProdutoAoCarrinho

--- 03/index.js
+++ 03/index.js
@@ -21,9 +21,43 @@ const carrinho = {
             quantidadeProdutos += produto.qtd;
             precoTotal += produto.qtd * produto.precoUnit; 
         }
-        console.log(`Cliente: ${this.nomeDoCliente} \nTotal de itens: ${quantidadeProdutos} \nTotal a pagar: R$ ${(precoTotal/100).toFixed(2).replace(".",",")}`);
+        console.log(`Cliente: ${this.nomeDoCliente} \nTotal de itens: ${quantidadeProdutos} ${quantidadeProdutos > 1 ? "itens" :"item"} \nTotal a pagar: R$ ${(precoTotal/100).toFixed(2).replace(".",",")}`);
 
     }
 }
 
+function addProdutoAoCarrinho(carrinho, produto){
+    const {produtos} = carrinho;
+    let noCarrinho = false;
+    for (let produtoCarrinho of produtos){
+        if (produtoCarrinho.id == produto.id){
+            produtoCarrinho.qtd += produto.qtd;
+            noCarrinho = true;
+            break;
+        }
+    }
+    if (!noCarrinho){
+        produtos.push(produto);
+    }
+}
+
+
+const novaBermuda = {
+    id: 2,
+    nome: "Bermuda",
+    qtd: 3,
+    precoUnit: 5000
+}
+
+addProdutoAoCarrinho(carrinho, novaBermuda);
+carrinho.imprimirResumo();
+
+const novoTenis = {
+    id: 3,
+    nome: "Tenis",
+    qtd: 1,
+    precoUnit: 10000
+}
+
+addProdutoAoCarrinho(carrinho, novoTenis);
 carrinho.imprimirResumo();
