fcc42e6ccad60b3d4baea612738a02e387c9fb8e
Karlasilvaeng <karlasilvaeng@gmail.com>
2023-07-12 18:23:15 -0300

    Implementação da função imprimirResumoDoCarrinho

--- 03/index.js
+++ 03/index.js
@@ -15,3 +15,16 @@ const carrinho = {
         }
     ]
 }
+
+function imprimirResumoDoCarrinho(carrinho){
+    const {nomeDoCliente, produtos} = carrinho;
+    let quantidadeProdutos = 0;
+    let precoTotal = 0;
+    for (let produto of produtos){
+        quantidadeProdutos += produto.qtd;
+        precoTotal += produto.qtd * produto.precoUnit; 
+    }
+    console.log(`Cliente: ${nomeDoCliente} \nTotal de itens: ${quantidadeProdutos} \nTotal a pagar: R$ ${(precoTotal/100).toFixed(2).replace(".",",")}`);
+}
+
+imprimirResumoDoCarrinho(carrinho);
